8eaf818231e2555a17433a0fa04f9826efa57d33
lluviagarcia <lluvia@uw.edu>
2022-07-08 09:11:44 -0700

    single event histogram

--- hello/vac.py
+++ hello/vac.py
@@ -1,6 +1,7 @@
 from asyncore import file_dispatcher
 import matplotlib.pyplot as plt
-
+import seaborn as sns
+import numpy as np
 
 energylist = []
 with open ("vac1.csv") as file:
@@ -8,16 +9,12 @@ with open ("vac1.csv") as file:
     while line[0] == "P" :
         energylist.append(line[6])
         line = file.readline().strip().split()
-        print(line[0])
-        if line == "": 
-            print("lineempty")
+        if len(line) == 0: 
             break
-        
-print(energylist)
+new_list = list(map(float, energylist))
 
 
+sns.histplot(data = new_list, color= 'navy', kde= 'true', log_scale=True)
+plt.xlabel('Energy')
+plt.show()
 
-
-
-
- 
